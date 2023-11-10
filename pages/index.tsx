270bf6dd6d33df5f325af7d64d4f5d05367d2603
lbo728 <72309817+lbo728@users.noreply.github.com>
2023-11-10 22:46:32 +0900

    next 캐시 제거

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-export default () => (
-  <div>
-    <h1>Hellsddssdfsfsfsffso</h1>
-  </div>
-);
+import type { NextPage } from "next";
+import Item from "@components/item";
+import Layout from "@components/layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="홈" hasTabBar>
+      <div className="flex flex-col space-y-5 divide-y">
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item
+            id={i}
+            key={i}
+            title="iPhone 14"
+            price={99}
+            comments={1}
+            hearts={1}
+          />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
